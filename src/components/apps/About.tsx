0baff844b0434082bdac6a7e0ba2a7338d49b14b
Vikrant <163637775+Vikrantz01@users.noreply.github.com>
2026-02-27 17:14:58 +0530

    fix: update image source path for profile picture in About component

--- src/components/apps/About.tsx
+++ src/components/apps/About.tsx
@@ -66,7 +66,7 @@ export function About({ isDark }: AboutProps) {
                 }`}
               >
                 <img
-                  src="/photo/Me.jpg"
+                  src="public/photo/Me.jpg"
                   alt=""
                 />
               </div>
