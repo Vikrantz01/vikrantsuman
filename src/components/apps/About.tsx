5ed82cba5cb9d48f9e6de8456c48aebeed456bd6
Vikrant <163637775+Vikrantz01@users.noreply.github.com>
2026-02-27 16:37:23 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/apps/About.tsx
+++ src/components/apps/About.tsx
@@ -66,7 +66,7 @@ export function About({ isDark }: AboutProps) {
                 }`}
               >
                 <img
-                  src="/public/photo/Photo.jpg"
+                  src="/public/photo/Me.jpg"
                   alt=""
                 />
               </div>
